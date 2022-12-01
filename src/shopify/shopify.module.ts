45b9327df6f83b2e3dd8cbe43ff575960d959e4e
Daniel Andrade <Daniel@conspireagency.com>
2022-11-30 22:12:48 -0800

    try modeling the module after mikroorm

--- src/shopify/shopify.module.ts
+++ src/shopify/shopify.module.ts
@@ -1,12 +1,48 @@
-import { Module } from "@nestjs/common";
-import { ConfigurableModuleClass } from "./shopify.module-definition";
+import {
+  DynamicModule,
+  Global,
+  Inject,
+  Module,
+  ModuleMetadata,
+} from "@nestjs/common";
 
 import { ProductService } from "./product.service";
 import { ShopService } from "./shop.service";
 
-@Module({
-  controllers: [],
-  providers: [ShopService, ProductService],
-  exports: [ShopService, ProductService],
-})
-export class ShopifyModule extends ConfigurableModuleClass {}
+//import { ConfigurableModuleClass } from "./shopify.module-definition";
+//import { MikroOrmModule } from "@mikro-orm/nestjs";
+
+// @Module({
+//   imports: [],
+//   controllers: [],
+//   providers: [ShopService, ProductService],
+//   exports: [ShopService, ProductService],
+// })
+// export class ShopifyModule extends ConfigurableModuleClass {}
+
+export interface ShopifyModuleAsyncOptions
+  extends Pick<ModuleMetadata, "imports" | "providers"> {
+  inject?: any[];
+}
+
+export const SHOPIFY_MODULE_OPTIONS = Symbol("shopify-module-options");
+
+@Global()
+@Module({})
+export class ShopifyModule {
+  constructor(
+    @Inject(SHOPIFY_MODULE_OPTIONS)
+    private readonly options: ShopifyModuleAsyncOptions
+  ) {}
+
+  static async forRootAsync(
+    options: ShopifyModuleAsyncOptions
+  ): Promise<DynamicModule> {
+    return {
+      module: ShopifyModule,
+      imports: options.imports || [],
+      providers: [ShopService, ProductService],
+      exports: [ShopService, ProductService],
+    };
+  }
+}
