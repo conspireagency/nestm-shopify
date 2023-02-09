ee5599678d40d010a5e063c0fbffd213ae2ee908
Daniel Andrade <Daniel@conspireagency.com>
2023-02-09 12:33:40 -0800

    update ShopDetail entity

--- src/shopify/entities/ShopDetail.entity.ts
+++ src/shopify/entities/ShopDetail.entity.ts
@@ -6,168 +6,162 @@ export class ShopDetail {
   @PrimaryKey({ type: ShopifyID })
   id: number;
 
-  @Property()
+  @Property({ nullable: true })
   address1: string;
 
-  @Property()
+  @Property({ nullable: true })
   address2: string;
 
-  @Property()
+  @Property({ nullable: true })
   checkout_api_supported: boolean;
 
-  @Property()
+  @Property({ nullable: true })
   city: string;
 
-  @Property()
-  country: string;
-
-  @Property()
-  country_code: string;
-
-  @Property()
-  country_name: string;
-
-  @Property()
-  county_taxes: any;
-
-  @Property()
-  created_at: Date;
-
-  @Property()
-  customer_email: string;
-
-  @Property()
-  currency: string;
-
-  @Property()
-  domain: string;
-
-  @Property()
-  enabled_presentment_currencies: string[];
-
-  @Property()
-  eligible_for_card_reader_giveaway: boolean;
-
-  @Property()
-  eligible_for_payments: boolean;
-
-  @Property()
-  email: string;
-
-  @Property()
-  finances: boolean;
-
-  @Property()
-  force_ssl: boolean;
-
-  @Property()
-  google_apps_domain: any;
-
-  @Property()
-  google_apps_login_enabled: any;
-
-  @Property()
-  has_discounts: boolean;
-
-  @Property()
-  has_gift_cards: boolean;
-
-  @Property()
-  has_storefront: boolean;
-
-  @Property()
-  iana_timezone: string;
-
-  @Property()
-  latitude: number;
-
-  @Property()
-  longitude: number;
-
-  @Property()
-  money_format: string;
-
-  @Property()
-  money_in_emails_format: string;
-
-  @Property()
-  money_with_currency_format: string;
-
-  @Property()
-  money_with_currency_in_emails_format: string;
-
-  @Property()
-  multi_location_enabled: boolean;
-
-  @Property()
-  myshopify_domain: string;
-
-  @Property()
-  name: string;
-
-  @Property()
-  password_enabled: boolean;
-
-  @Property()
-  phone: any;
-
-  @Property()
-  plan_display_name: string;
-
-  @Property()
-  pre_launch_enabled: boolean;
-
-  @Property()
+  @Property({ nullable: true })
   cookie_consent_level: string;
 
-  @Property()
+  @Property({ nullable: true })
+  country: string;
+
+  @Property({ nullable: true })
+  country_code: string;
+
+  @Property({ nullable: true })
+  country_name: string;
+
+  @Property({ nullable: true })
+  county_taxes: boolean;
+
+  @Property({ nullable: true })
+  created_at: string;
+
+  @Property({ nullable: true })
+  currency: string;
+
+  @Property({ nullable: true })
+  customer_email: string;
+
+  @Property({ nullable: true })
+  domain: string;
+
+  @Property({ nullable: true })
+  eligible_for_card_reader_giveaway: boolean;
+
+  @Property({ nullable: true })
+  eligible_for_payments: boolean;
+
+  @Property({ nullable: true })
+  email: string;
+
+  @Property({ nullable: true })
+  enabled_presentment_currencies: string[];
+
+  @Property({ nullable: true })
+  finances: boolean;
+
+  @Property({ nullable: true })
+  force_ssl: boolean;
+
+  @Property({ nullable: true })
+  google_apps_domain: string;
+
+  @Property({ nullable: true })
+  google_apps_login_enabled: boolean;
+
+  @Property({ nullable: true })
+  has_discounts: boolean;
+
+  @Property({ nullable: true })
+  has_gift_cards: boolean;
+
+  @Property({ nullable: true })
+  has_storefront: boolean;
+
+  @Property({ nullable: true })
+  iana_timezone: string;
+
+  @Property({ nullable: true })
+  latitude: number;
+
+  @Property({ nullable: true })
+  longitude: number;
+
+  @Property({ nullable: true })
+  money_format: string;
+
+  @Property({ nullable: true })
+  money_in_emails_format: string;
+
+  @Property({ nullable: true })
+  money_with_currency_format: string;
+
+  @Property({ nullable: true })
+  money_with_currency_in_emails_format: string;
+
+  @Property({ nullable: true })
+  myshopify_domain: string;
+
+  @Property({ nullable: true })
+  name: string;
+
+  @Property({ nullable: true })
+  password_enabled: boolean;
+
+  @Property({ nullable: true })
+  phone: string;
+
+  @Property({ nullable: true })
+  plan_display_name: string;
+
+  @Property({ nullable: true })
   plan_name: string;
 
-  @Property()
+  @Property({ nullable: true })
+  pre_launch_enabled: boolean = false;
+
+  @Property({ nullable: true })
   primary_locale: string;
 
-  @Property()
+  @Property({ nullable: true })
   primary_location_id: number;
 
-  @Property()
+  @Property({ nullable: true })
   province: string;
 
-  @Property()
+  @Property({ nullable: true })
   province_code: string;
 
-  @Property()
-  requires_extra_payments_agreement: boolean;
+  @Property({ nullable: true })
+  requires_extra_payments_agreement: boolean = false;
 
-  @Property()
-  setup_required: boolean;
+  @Property({ nullable: true })
+  setup_required: boolean = true;
 
-  @Property()
+  @Property({ nullable: true })
   shop_owner: string;
 
-  @Property()
-  source: any;
+  @Property({ nullable: true })
+  source: string | null;
 
-  @Property()
-  taxes_included: any;
+  @Property({ nullable: true })
+  tax_shipping: boolean | null;
 
-  @Property()
-  tax_shipping: any;
+  @Property({ nullable: true })
+  taxes_included: true | null;
 
-  @Property()
+  @Property({ nullable: true })
   timezone: string;
 
-  @Property()
+  @Property({ nullable: true })
   transactional_sms_disabled: boolean;
 
-  @Property()
+  @Property({ nullable: true })
   updated_at: Date;
 
-  @Property()
+  @Property({ nullable: true })
   weight_unit: string;
 
-  @Property()
+  @Property({ nullable: true })
   zip: string;
-
-  @Property()
-  marketing_sms_consent_enabled_at_checkout: boolean;
 }
